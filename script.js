edada05d6a0f32e58c226c4856a22cc761db40c6
geekbrains <info@geekbrains.ru>
2023-01-21 21:36:26 +0300

    save

---
+++ script.js
@@ -0,0 +1,7 @@
+let temperaturC = prompt('Введите температуру в С');
+let far = (9 / 5) * temperaturC + 32;
+console.log(temperaturC, far);
+
+let name = "Victor";
+let admin = name;
+console.log(admin);
